01a82ec4cb55f5e971ce512f3a4607e0d6c80852
Byron-Selvage <Byron.Selvage@nlr.gov>
2026-07-06 14:12:53 -0600

    Fix CNN LR Schedule - Change from ChainedScheduler to SequentialLR

--- routee/powertrain/trainers/cnn.py
+++ routee/powertrain/trainers/cnn.py
@@ -234,8 +234,10 @@ class CNNTrainer(Trainer):
                     start_factor=1e-8,
                     total_iters=warmup_steps,
                 )
-                scheduler = torch.optim.lr_scheduler.ChainedScheduler(
-                    [warmup_scheduler, cosine_scheduler]
+                scheduler = torch.optim.lr_scheduler.SequentialLR(
+                    optimizer,
+                    schedulers=[warmup_scheduler, cosine_scheduler],
+                    milestones=[warmup_steps],
                 )
             else:
                 scheduler = cosine_scheduler
